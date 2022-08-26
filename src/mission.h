b787333cbdeb5162f6968262e7df758fd6cf308d
WangMahua <whole9177j53@gmail.com>
2022-08-26 11:05:26 +0800

    delete useless function and add print function

--- src/mission.h
+++ src/mission.h
@@ -35,9 +35,9 @@ public:
     void px4_callback(const auto_flight::ncrl_link::ConstPtr& msg);
 	void pos_callback(const geometry_msgs::PoseStamped::ConstPtr&);
 	void web_phase_callback(const std_msgs::Int8::ConstPtr&);
-	void land_callback(const std_msgs::Int8::ConstPtr&);
 	void waypoint_callback(const geometry_msgs::Vector3::ConstPtr&);
     void publisher();
+    void print_current_state();
     bool mission_switch();
     void mission_insert(string m, string a, float d1,float d2, float d3);
     void process();
@@ -48,9 +48,7 @@ MISSION::MISSION(){
 	get_px4_data_sub = n.subscribe<auto_flight::ncrl_link>("pixhawk_to_pc",1,&MISSION::px4_callback,this);
 	pos_sub = n.subscribe<geometry_msgs::PoseStamped>("/vrpn_client_node/MAV1/pose", 10, &MISSION::pos_callback, this); 
 	web_phase_sub = n.subscribe<std_msgs::Int8>("/control_phase", 1, &MISSION::web_phase_callback, this); 
-    land_sub = n.subscribe<std_msgs::Int8>("/land", 1, &MISSION::land_callback, this); 
 	web_waypoint_sub = n.subscribe<geometry_msgs::Vector3>("/flight_destination", 1, &MISSION::waypoint_callback, this); 
-
 
     /* init */
     mission_data.mode = "0";
@@ -62,7 +60,8 @@ MISSION::MISSION(){
     /* If you want to add waypoint you can edit here */
     /* mission_insert( string mode, string aux_info, */
     /*      \  float data1,float data2, float data3) */
-    /* Please check readme.md           */
+    /* ex : mission_insert("2", "0", 1.0, 1.0, 0.0); */
+    /* Please check readme.md                        */
 
     mission_insert("0", "0", 0.0, 0.0, 0.0);
 }
@@ -83,11 +82,6 @@ void MISSION::web_phase_callback(const std_msgs::Int8::ConstPtr& msg){
     }else if(msg->data == 3){
         mission_insert("3", "0", 0.0, 0.0, 0.0);
     }
-}
-
-void MISSION::land_callback(const std_msgs::Int8::ConstPtr& msg){
-    
-
 }
 
 void MISSION::waypoint_callback(const geometry_msgs::Vector3::ConstPtr& msg){
@@ -166,11 +160,26 @@ void MISSION::mission_insert(string m, string a, float d1,float d2, float d3){
     final_task +=1;
 }
 
-void MISSION::process(){
+void MISSION::print_current_state(){
     cout << " cur_task : " << cur_task <<endl;
-    cout << " final_task : " << final_task <<endl;
-    cout << " === " << endl;
+    string mode = mission_list[cur_task].mode;
+    if(mode == "1"){
+        cout << "takeoff " << endl;
+    }else if(mode == "2"){
+        cout << "waypoint ->"<<" target_x : " << mission_list[cur_task].data1 << " target_y : " << mission_list[cur_task].data2 << endl;
+    }else if(mode == "3"){
+        cout << "landing " << endl;
+    }else{
+        cout << "wait cmd "<<endl;
+    }
 
+    cout << " total task num : " << final_task <<endl;
+    cout << " === " << endl;
+}
+
+void MISSION::process(){
+
+    print_current_state();
 
     if(cur_task!=final_task){
         if(mission_switch()==true && cur_task!=final_task-1){ // If this mission is not the last one.
